d669a348c15426e2674f44cfebcf8c686baea10a
Charlie <itscharlieliu@gmail.com>
2023-07-08 18:55:29 -0700

    hit openai endpoint and get a result

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/itscharlieliu/ingredient-ai-backend
 
 go 1.20
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/sashabaranov/go-openai v1.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	openai "github.com/sashabaranov/go-openai"
 )
 
+// FOR TESTING
 func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
     fmt.Println("Endpoint Hit: homePage")
+
+	resp, err := getResponseFromChatGpt("say you are testing this endpoint")
+
+	if (err != nil) {
+		fmt.Println("Error")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Fprintf(w, resp)
 }
 
+func getResponseFromChatGpt(prompt string) (string, error){
+	openAiApiKey := os.Getenv("OPENAI_API_KEY")
+    fmt.Println(openAiApiKey)
+
+	client := openai.NewClient(openAiApiKey)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return "", err
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
+ 
+
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+
+	router := mux.NewRouter().StrictSlash(true)
+
+    router.HandleFunc("/", homePage)
+
+    log.Fatal(http.ListenAndServe(":3259", router))
 }
 
 func main() {
+
+	fmt.Println(os.Getenv("OPENAI_API_KEY"))
+
     handleRequests()
 }
